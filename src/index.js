140a962b0c8bbd25865590a9915af9375c4d9d3e
Egor Dubovik <Hamelion74@gmail.com>
2020-03-06 01:32:49 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-    
+    if (n < 0) {
+        n = n * (-1);
+        n = String(n);
+        return n.split("").reverse().join("");
+    } else {
+        n = String(n);
+        return n.split("").reverse().join("");
+    }
 };
